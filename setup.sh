90f022aea61efead9153b0e1a6919c725abba1b0
Adrian Oprea <oprea@hey.com>
2025-06-05 16:08:13 +0300

    Extra configs

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-yay -Sy zsh
+yay -Sy zsh hyprland hyprshot hyprpaper hypridle hyprlock wayland waypaper swaync
 sudo chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# Install apps
+
+yay -Sy gimp obs-studio vlc
 
